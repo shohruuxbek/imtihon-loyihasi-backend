63b35475889ced1792ff5c041911615ed0c7f30e
shohruuxbek <qadamboyevshohrux6@gmail.com.com>
2026-04-05 23:31:18 +0500

    update: nimani o‘zgartirdingiz

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { StudentsModule } from './students/students.module.js';
@@ -18,13 +11,9 @@ import { AuthModule } from './auth/auth.module.js';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: process.env.NODE_ENV === 'production' ? 'postgres' : 'sqlite',
-      url: process.env.DATABASE_URL,
-      database: process.env.NODE_ENV !== 'production' ? 'oquv_markazi.db' : undefined,
-      entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
-      synchronize: true, // Production'da false qiling va migrations ishlating
-      logging: process.env.NODE_ENV === 'development',
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }),
     StudentsModule,
     TeachersModule,
